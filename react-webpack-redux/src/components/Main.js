25abbba14198fc50d6cbf8f64f482a7e38b00e9e
Samurais <hailiang.hl.wang@gmail.com>
2016-06-03 17:46:41 +0800

    Move bootstrap into src/components/Foo1Component.js

--- react-webpack-redux/src/components/Main.js
+++ react-webpack-redux/src/components/Main.js
@@ -2,32 +2,26 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
-import { ButtonToolbar, Button } from 'react-bootstrap';
+
 import Foo1Component from './Foo1Component';
 
 let yeomanImage = require('../images/yeoman.png');
 
-class AppComponent extends React.Component {
+class AppComponent extends React.Component<any, any> {
+    
+    constructor(props){
+        super(props);
+        super.state = {count: parseInt(props.initialVal)};
+        console.log(props)
+    }
+    
     render() {
-        return ( < div className = "index" >
+        return ( 
+            < div className = "index" >
+              <Foo1Component> </Foo1Component>
             < img src = { yeomanImage }
             alt = "Yeoman Generator" / >
             < div className = "notice" > Please edit < code > src / components / Main.js < /code> to get started!</div >
-
-            < ButtonToolbar > { /* Standard button */ } < Button > Default < /Button>
-
-            { /* Provides extra visual weight and identifies the primary action in a set of buttons */ } < Button bsStyle = "primary" > Primary < /Button>
-
-            { /* Indicates a successful or positive action */ } < Button bsStyle = "success" > Success < /Button>
-
-            { /* Contextual button for informational alert messages */ } < Button bsStyle = "info" > Info < /Button>
-
-            { /* Indicates caution should be taken with this action */ } < Button bsStyle = "warning" > Warning < /Button>
-
-            { /* Indicates a dangerous or potentially negative action */ } < Button bsStyle = "danger" > Danger < /Button>
-
-            { /* Deemphasize a button by making it look like a link while maintaining button behavior */ } < Button bsStyle = "link" > Link < /Button> < /ButtonToolbar>
-               <Foo1Component> </Foo1Component>
             < /div>
         );
     }
